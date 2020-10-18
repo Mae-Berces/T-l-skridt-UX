91881b1c08310dd2ef1ee422cf6f8d38a0bd9687
Mae-Berces <eaajmmb@students.eaaa.dk>
2020-10-18 22:34:46 +0200

    Update på login tilføjet js.js

---
+++ js.js
@@ -0,0 +1,17 @@
+/*
+ * fil: js.js
+ * purpose: introdction to jQuery
+ */
+console.log('file: js/js.js loaded');
+
+// A $( document ).ready() block.
+
+
+// ... codeing from here ...
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
